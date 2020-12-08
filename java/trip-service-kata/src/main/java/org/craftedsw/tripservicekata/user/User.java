4f81e07a72adcebeaf6227a6a21e19b0ef575571
edu05 <eduardo.winpenny@gmail.com>
2020-12-09 00:33:46 +0100

    added builder for convenience

--- java/trip-service-kata/src/main/java/org/craftedsw/tripservicekata/user/User.java
+++ java/trip-service-kata/src/main/java/org/craftedsw/tripservicekata/user/User.java
@@ -13,17 +13,38 @@ public class User {
 	public List<User> getFriends() {
 		return friends;
 	}
-	
-	public void addFriend(User user) {
-		friends.add(user);
-	}
 
-	public void addTrip(Trip trip) {
-		trips.add(trip);
-	}
-	
 	public List<Trip> trips() {
 		return trips;
 	}
 
+
+	public static final class UserBuilder {
+		private List<Trip> trips = new ArrayList<Trip>();
+		private List<User> friends = new ArrayList<User>();
+
+		private UserBuilder() {
+		}
+
+		public static UserBuilder anUser() {
+			return new UserBuilder();
+		}
+
+		public UserBuilder withTrips(List<Trip> trips) {
+			this.trips = trips;
+			return this;
+		}
+
+		public UserBuilder withFriends(List<User> friends) {
+			this.friends = friends;
+			return this;
+		}
+
+		public User build() {
+			User user = new User();
+			user.trips = this.trips;
+			user.friends = this.friends;
+			return user;
+		}
+	}
 }
