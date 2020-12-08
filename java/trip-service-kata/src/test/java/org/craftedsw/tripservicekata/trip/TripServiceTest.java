8d049e7c6bc9bbf33267e564f8f435e08c0e1c52
edu05 <eduardo.winpenny@gmail.com>
2020-12-09 00:57:09 +0100

    third test

--- java/trip-service-kata/src/test/java/org/craftedsw/tripservicekata/trip/TripServiceTest.java
+++ java/trip-service-kata/src/test/java/org/craftedsw/tripservicekata/trip/TripServiceTest.java
@@ -7,6 +7,7 @@ import org.junit.jupiter.api.Test;
 import java.util.ArrayList;
 import java.util.List;
 
+import static org.craftedsw.tripservicekata.user.User.UserBuilder.anUser;
 import static org.hamcrest.MatcherAssert.assertThat;
 import static org.hamcrest.Matchers.is;
 import static org.junit.jupiter.api.Assertions.assertThrows;
@@ -14,6 +15,7 @@ import static org.junit.jupiter.api.Assertions.assertThrows;
 public class TripServiceTest {
 
     private static User loggedInUser;
+    private static List<Trip> trips;
 
     @Test
     public void shouldThrowExceptionWhenUserIsNotLoggedIn() {
@@ -32,10 +34,33 @@ public class TripServiceTest {
         assertThat(tripsDeLaExDeEdu.size(), is(0));
     }
 
+    @Test
+    void shouldReturnTripsWhenUserAndLoggedInUserAreFriends() {
+        loggedInUser = new User(); //representa a Edu
+        Trip tripToSanFran = new Trip();
+        Trip tripToThailand = new Trip();
+        trips = List.of(tripToSanFran, tripToThailand);
+        User javi = anUser()
+                .withFriends(List.of(loggedInUser))
+                .build();
+
+        List<Trip> tripsDeJavi = new TestableTripService().getTripsByUser(javi);
+
+        assertThat(tripsDeJavi.size(), is(2));
+        assertThat(tripsDeJavi.get(0), is(tripToSanFran));
+        assertThat(tripsDeJavi.get(1), is(tripToThailand));
+    }
+
     private static class TestableTripService extends TripService {
+
         @Override
         protected User getLoggedUser() {
             return loggedInUser;
+        }
+
+        @Override
+        protected List<Trip> findTripsByUser(User user) {
+            return trips;
         }
     }
 
